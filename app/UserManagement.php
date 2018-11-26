14012d5741374f70fbe116863361d452c8f8e54f
Bartosz <rbartek.r100@gmail.com>
2018-11-26 12:09:51 +0100

    Update UserManagement model

--- app/UserManagement.php
+++ app/UserManagement.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserManagement extends Model
 {
+	private $id;
 	private $name;
 	private $surname;
 	private $email;
@@ -17,6 +18,15 @@ class UserManagement extends Model
        	]);
 
 		$this->id = $id;
+		
+		$user = DB::table('users')
+					->where('id', $this->id)
+					->get();
+
+		$this->name = $user->name;
+		$this->surname = $user->surname;
+		$this->email = $user->email;
+		$this->password = $user->password;
 	}
 
     public function getName() {
@@ -25,14 +35,55 @@ class UserManagement extends Model
 
     public function setName($name) {
 	    $name->validate([
-           'name' => 'required|max:25',
+           'name' => 'required|string|max:25',
        	]);
 
         $this->name = $name;
     }
 
-    public function save(array $data = array())
+    public function getSurname() {
+        return $this->surname;
+    }
+
+    public function setSurname($surname) {
+	    $surname->validate([
+           'surname' => 'required|string|max:35',
+       	]);
+
+        $this->surname = $surname;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setEmail($email) {
+	    $email->validate([
+           'email' => 'required|email|max:50',
+       	]);
+
+        $this->email = $email;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function setPassword($password) {
+	    $password->validate([
+           'password' => 'required|max:25',
+       	]);
+
+        $this->password = $password;
+    }
+
+    public function save()
 	{   
-        parent::save($data);
+        DB::table('users')
+           ->where('id', $this->id)
+           ->update(['name' => $this->name])
+           ->update(['surname' => $this->surname])
+           ->update(['email' => $this->email])
+           ->update(['password' => $this->password]);
     }
 }
