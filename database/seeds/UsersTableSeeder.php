3e0d2ae15706894c6f08c6094a98b82418051a9e
Artur Lisowski <artur.lisowski@rom.be>
2018-12-16 16:45:53 +0100

    UsersYableSeeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    
+    $_faker = Faker::create();
+    
+    for($i=0;$i<20;$i++) {
+        DB::table('users')->insert([
+            'name' => $_faker->name,
+            'email' => $_faker->email,
+            'password' => bcrypt('password'),
+            'active' => rand(0, 1)
+        ]);
+    }
+        DB::table('users')->insert([
+            'name' => 'sys administrator',
+            'email' => 'administrator@gmail.com',
+            'password' => bcrypt('123qwe'),
+        ]);
+    }
+
+}
