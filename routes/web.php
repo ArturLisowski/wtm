e8b4492cd96f877be32d9153f9984ea82ec01ec3
Artur Lisowski <artur.lisowski@rom.be>
2018-12-16 16:45:34 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/userManagement', 'UserManagementController@index')->name('userManagement');
+
