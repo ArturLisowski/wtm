d8f427212b8b57b6b2158836ea8e0400ca8bd41d
Artur Lisowski <artur.lisowski@rom.be>
2018-12-16 17:07:04 +0100

    Update UserManagement.php

--- app/UserManagement.php
+++ app/UserManagement.php
@@ -3,73 +3,101 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class UserManagement extends Model
 {
-	private $id;
-	private $name;
-	private $email;
-	private $password;
-
-	public function __construct($id) {
-		$id->validate([
-           'id' => 'integer',
-       	]);
-
-		$this->id = $id;
-		
-		$user = DB::table('users')
-					->where('id', $this->id)
-					->get();
-
-		$this->name = $user->name;
-		$this->email = $user->email;
-		$this->password = $user->password;
-	}
-
-    public function getName() {
+    private $id;
+    private $name;
+    private $email;
+    private $password;
+    
+    public function __construct($id)
+    {
+        $id->validate([
+            'id' => 'integer',
+        ]);
+        
+        $this->id = $id;
+        
+        $user = DB::table('users')
+            ->where('id', $this->id)
+            ->get();
+        
+        $this->name = $user->name;
+        $this->email = $user->email;
+        $this->password = $user->password;
+    }
+    
+    public function getName()
+    {
         return $this->name;
     }
-
-    public function setName($name) {
-	    $name->validate([
-           'name' => 'required|string|max:25',
-       	]);
-
+    
+    public function setName($name)
+    {
+        $name->validate([
+            'name' => 'required|string|max:25',
+        ]);
+        
         $this->name = $name;
     }
-
-    public function getEmail() {
+    
+    public function getEmail()
+    {
         return $this->email;
     }
-
-    public function setEmail($email) {
-	    $email->validate([
-           'email' => 'required|email|max:50',
-       	]);
-
+    
+    public function setEmail($email)
+    {
+        $email->validate([
+            'email' => 'required|email|max:50',
+        ]);
+        
         $this->email = $email;
     }
-
-    public function getPassword() {
+    
+    public function getPassword()
+    {
         return $this->password;
     }
-
-    public function setPassword($password) {
-	    $password->validate([
-           'password' => 'required|max:25',
-       	]);
-
+    
+    public function setPassword($password)
+    {
+        $password->validate([
+            'password' => 'required|max:25',
+        ]);
+        
         $this->password = $password;
     }
 
-    public function save()
-	{   
-        DB::table('users')
-           ->where('id', $this->id)
-           ->update(['name' => $this->name])
-           ->update(['surname' => $this->surname])
-           ->update(['email' => $this->email])
-           ->update(['password' => $this->password]);
+//    public function save()
+//	{
+//        DB::table('users')
+//           ->where('id', $this->id)
+//           ->update(['name' => $this->name])
+//           ->update(['surname' => $this->surname])
+//           ->update(['email' => $this->email])
+//           ->update(['password' => $this->password]);
+//    }
+    
+    public static function getAllUsers($paginate = null)
+    {
+        if (!empty($paginate)) {
+            return DB::table('users')->select('*')->paginate($paginate);
+        } else {
+            return DB::table('users')->select('*')->get();
+        }
+        
+    }
+    
+    public static function getAllActiveUsers($paginate = null)
+    {
+        if (!empty($paginate)) {
+            return DB::table('users')->select('*')->where('active', '=', 1)->paginate($paginate);
+        } else {
+            return DB::table('users')->select('*')->where('active', '=', 1)->get();
+        }
+        
     }
 }
