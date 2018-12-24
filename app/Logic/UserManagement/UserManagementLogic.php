d6f729053949867066cdfeda6b866c6fffba2cae
Artur Lisowski <artur.lisowski@rom.be>
2018-12-24 16:17:36 +0100

    Create UserManagementLogic.php

---
+++ app/Logic/UserManagement/UserManagementLogic.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: artur
+ * Date: 24.12.2018
+ * Time: 15:57
+ */
+
+namespace App\Logic\UserManagement;
+
+
+use App\UserManagement;
+
+class UserManagementLogic extends UserManagement
+{
+    public static function changeActive(int $userId)
+    {
+    
+    }
+}
