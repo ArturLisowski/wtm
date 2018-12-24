24f7775caf9309fccc8d86e2bd8a63685753f049
Artur Lisowski <artur.lisowski@rom.be>
2018-12-24 16:17:27 +0100

    Update UserManagementController.php

--- app/Http/Controllers/UserManagementController.php
+++ app/Http/Controllers/UserManagementController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Logic\UserManagement\UserManagementLogic;
 use App\UserManagement;
-use Illuminate\Http\Request;
+
 
 class UserManagementController extends Controller
 {
@@ -12,10 +13,16 @@ class UserManagementController extends Controller
         $this->middleware('auth');
     }
     
-    public function index($orderBy = 'id' , $method = 'ASC')
+    public function index(string $orderBy = 'id' ,string $method = 'ASC') //todo validateInput
     {
         $users = UserManagement::getAllUsers(15,$orderBy,$method);
         $formMethod = ($method == 'ASC')? 'DESC' : 'ASC';
         return view('userManagement/index', compact('users','formMethod','orderBy'));
     }
+    
+    public function setActive(int $id) //todo validateInput
+    {
+        UserManagementLogic::changeActive($id);
+    }
+    
 }
