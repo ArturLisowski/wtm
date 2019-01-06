096a4adaf1beb410ebef6e72a78b2140e6b7b59a
Artur Lisowski <artur@lisart.pl>
2019-01-06 12:03:44 +0100

    Update WorkingTime.php

--- app/WorkingTime.php
+++ app/WorkingTime.php
@@ -3,14 +3,49 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class WorkingTime extends Model
 {
+    private $id;
     private $userId;
     private $data;
     private $start;
     private $end;
     private $workingTime;
+    
+    public function __construct(int $id = 0)
+    {
+        if ($id > 0) {
+            $data = DB::table('working_times')
+                ->select('*')
+                ->where('id', '=', $id)
+                ->get();
+            $this->id = $data[0]->id;
+            $this->userId = $data[0]->userId;
+            $this->data = $data[0]->data;
+            $this->start = $data[0]->start;
+            $this->end = $data[0]->end;
+            $this->workingTime = $data[0]->workingTime;
+        }
+    }
+    
+    /**
+     * @param $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
     
     /**
      * @param int $userId
@@ -74,5 +109,38 @@ class WorkingTime extends Model
     public function getWorkingTime()
     {
         return $this->workingTime;
+    }
+    
+    /**
+     * @param array $options
+     * @return bool|void
+     */
+    public function save(array $options = [])
+    {
+        if ($this->id > 0) {
+            DB::table('working_times')
+                ->where('id', $this->id)
+                ->update([
+                    'updated_at' => date('Y-m-d H:i:s'),
+                    'userId' => $this->userId,
+                    'data' => $this->data,
+                    'start' => $this->start,
+                    'end' => $this->end,
+                    'workingTime' => $this->workingTime
+                ]);
+        } else {
+            DB::table('working_times')
+                ->insert(
+                    [
+                        'created_at' => date('Y-m-d H:i:s'),
+                        'updated_at' => date('Y-m-d H:i:s'),
+                        'userId' => $this->userId,
+                        'data' => $this->data,
+                        'start' => $this->start,
+                        'end' => $this->end,
+                        'workingTime' => $this->workingTime
+                    ]
+                );
+        }
     }
 }
